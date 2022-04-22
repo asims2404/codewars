9051b265249a4a67ed7bf56b5f28989f931034ab
Ashley Sims <asims2404@gmail.com>
2022-04-22 23:36:11 +0100

    add 2022-04-22 kata5 the Hashing Generator

---
+++ 2022-04-22-kata5-theHashingGenerator.js
@@ -0,0 +1,35 @@
+/* 
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+    It must start with a hashtag (#).
+    All words must have their first letter capitalized.
+    If the final result is longer than 140 chars it must return false.
+    If the input or the result is an empty string it must return false.
+    remove spaces
+Examples
+
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+
+function generateHashtag(str) {
+  const arrCopy = str.split(" ");
+  let strCopy = "";
+  arrCopy.map((w) => {
+    if (w) {
+      strCopy += w[0].toUpperCase() + w.slice(1);
+    }
+  });
+  if (!strCopy.length || strCopy.length > 139) {
+    return false;
+  }
+  return "#" + strCopy;
+}
+
+console.log(generateHashtag("   "));
+console.log(generateHashtag("    hello     World   "));
+//console.log(generateHashtag(" Hello there thanks for trying my Kata"));
